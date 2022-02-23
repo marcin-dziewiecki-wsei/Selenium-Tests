7e2de8768008058bbacb22f6226096e0497d9dd3
Marcin Dziewięcki <marcin@dziewiecki.net>
2022-02-23 02:07:31 +0100

    End of part 2

--- SeleniumTests/BaseClass/BaseTest.cs
+++ SeleniumTests/BaseClass/BaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace SeleniumTests.BaseClass
 {
@@ -8,7 +9,7 @@ namespace SeleniumTests.BaseClass
     {
         public IWebDriver driver;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Open()
         {
             driver = new ChromeDriver();
@@ -17,9 +18,14 @@ namespace SeleniumTests.BaseClass
 
             // Accept cookie popup
             driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]")).Click();
+            Thread.Sleep(500);
+
+            // Open create new account popup
+            driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a")).Click();
+            Thread.Sleep(500);
         }
 
-        [OneTimeTearDown]
+        [TearDown]
         public void Close()
         {
             driver.Quit();
--- SeleniumTests/TestClass.cs
+++ SeleniumTests/TestClass.cs
@@ -2,35 +2,43 @@
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SeleniumTests.BaseClass;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SeleniumTests
 {
     [TestFixture]
     public class TestClass: BaseTest
     {
-        [Test]
+        [Test, Category("Smoke Testing")]
         public void TestMethod1()
         {
-            var emailTextField = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));
+            var emailTextField = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input"));
             emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(500);
+
+            var monthDropdownList = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[2]"));
+            var element = new SelectElement(monthDropdownList);
+            element.SelectByIndex(4);
+            element.SelectByText("gru");
+            element.SelectByValue("7");
+
             //Thread.Sleep(2000);
         }
 
-        [Test]
+        [Test, Category("Regression Testing")]
         public void TestMethod2()
         {
-            var emailTextField = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));
+            var emailTextField = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input"));
             emailTextField.SendKeys("Selenium C#");
             //Thread.Sleep(2000);
         }
 
-        [Test]
+        [Test, Category("Smoke Testing")]
         public void TestMethod3()
         {
-            var emailTextField = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));
+            var emailTextField = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input"));
             emailTextField.SendKeys("Selenium C#");
             //Thread.Sleep(2000);
         }
